9a8d8a2d6fc10d9957b89974c7c791b98a0e1e5b
GaRrRi4308 <romakuriluk@gmail.com>
2023-02-21 19:51:14 +0200

    add IsLoggedIn

---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+bool IsLoggedIn() { //
+    string username, password;
+
+    cout << "Enter username: "; cin >> username;
+    cout << "Enter password: "; cin >> password;
+
+    return false;
+}
+
+int main() {
+    IsLoggedIn();
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
